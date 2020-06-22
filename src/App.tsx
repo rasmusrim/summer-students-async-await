cd973e644b20a4e9aeaa8e7a5eb57dfb3aa57974
Rasmus Rimestad <rasmus.rimestad@itera.no>
2020-06-22 19:50:06 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const rootUrl = "https://football-rest-api.azurewebsites.net";
+
+async function fetchPlayerFavoriteColorWithAsyncAwait(): Promise<string> {
+  let playerResponse = await fetch(rootUrl + "/player");
+
+  let playerIds: number[] = await playerResponse.json();
+  let playerId: number = playerIds[0];
+
+  playerResponse = await fetch(rootUrl + "/player/" + playerId);
+  let playerDetails = await playerResponse.json();
+
+  let colorResponse = await fetch(rootUrl + "/color/" + playerDetails.favoriteColorId);
+  let color = await colorResponse.json();
+
+  return color.colorName;
+
+}
+
+function fetchPlayerFavoriteColor() {
+  fetch(rootUrl + "/player")
+  .then(response => response.json())
+  .then((response: number[]) => {
+    fetch(rootUrl + "/player/" + response[0])
+    .then(response => response.json())
+    .then(response => {
+      fetch(rootUrl + "/color/" + response.favoriteColorId)
+      .then(response =>   response.json())
+      .then(response => console.log(response))
+    })
+  });
+}
+
 function App() {
+   
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
